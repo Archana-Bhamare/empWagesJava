ba386e026d6d89a9299b7f4712e3221e066b8db5
Archana-Bhamare <archusonar2@gmail.com>
2020-10-20 17:27:08 +0530

    Added : Solving using switch case statement

--- src/com/brigelabz/Employee.java
+++ src/com/brigelabz/Employee.java
@@ -2,21 +2,25 @@ package com.brigelabz;
 
 public class Employee {
 	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
+		/*int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;*/
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-		if (empCheck == IS_FULL_TIME) {
+		switch (empCheck) {
+		case 1:
 			System.out.println("Employee is Present for full time");
 			empHrs = 8;
-		} else if (empCheck == IS_PART_TIME) {
+			break;
+		case 2:
 			System.out.println("Employee is Present for part time");
 			empHrs = 4;
-		} else {
-			System.out.println("EMployee is Absent");
+			break;
+		default:
+			System.out.println("Employee is Absent");
 			empHrs = 0;
+			break;
 		}
 		int dailyWage = EMP_RATE_PER_HOUR * empHrs;
 		System.out.println("Employee Daily wage is : " + dailyWage);
