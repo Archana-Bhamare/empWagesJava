3e4a5b653342fcac7e17d072070cdd01464b7c06
Archana-Bhamare <archusonar2@gmail.com>
2020-10-20 16:33:09 +0530

    Added : Calculate Daily Employee Wages

--- src/com/brigelabz/Employee.java
+++ src/com/brigelabz/Employee.java
@@ -3,11 +3,18 @@ package com.brigelabz;
 public class Employee {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
-		int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
-		 if (empCheck == IS_FULL_TIME ) {
-			 System.out.println("Employee is present");
-		 } else {
-			 System.out.println("Employee is absent");
-		 }
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs=0;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+		
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("EMployee is Present"); 
+			empHrs = 8;
+		} else {
+			System.out.println("EMployee is Absent"); 
+			empHrs = 0;
+		}
+		int dailyWage = EMP_RATE_PER_HOUR *empHrs;
+		System.out.println("Employee Daily wage is : " + dailyWage);
 	}
 }
