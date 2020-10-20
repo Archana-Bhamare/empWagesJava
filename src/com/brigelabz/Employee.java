029ec76efc95aa110425533abd8b9e08697b6263
Archana-Bhamare <archusonar2@gmail.com>
2020-10-20 17:44:56 +0530

    Added : Calculating wages for a Month

--- src/com/brigelabz/Employee.java
+++ src/com/brigelabz/Employee.java
@@ -2,27 +2,29 @@ package com.brigelabz;
 
 public class Employee {
 	public static void main(String[] args) {
-		/*int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;*/
+		/*
+		 * int IS_FULL_TIME = 1; int IS_PART_TIME = 2;
+		 */
 		int EMP_RATE_PER_HOUR = 20;
+		int NUM_OF_WORKING_DAYS = 20;
 		int empHrs = 0;
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-
-		switch (empCheck) {
-		case 1:
-			System.out.println("Employee is Present for full time");
-			empHrs = 8;
-			break;
-		case 2:
-			System.out.println("Employee is Present for part time");
-			empHrs = 4;
-			break;
-		default:
-			System.out.println("Employee is Absent");
-			empHrs = 0;
-			break;
+		int totalSalary = 0;
+		for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (empCheck) {
+			case 1:
+				empHrs = 8;
+				break;
+			case 2:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+				break;
+			}
+			int dailyWage = EMP_RATE_PER_HOUR * empHrs;
+			totalSalary = totalSalary + dailyWage;
 		}
-		int dailyWage = EMP_RATE_PER_HOUR * empHrs;
-		System.out.println("Employee Daily wage is : " + dailyWage);
+		System.out.println("Total Salary for a month is : " + totalSalary);
 	}
 }
