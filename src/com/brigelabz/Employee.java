9bcf9f226854eb84b3feca8ea566b6239ed06ae3
Archana-Bhamare <archusonar2@gmail.com>
2020-10-20 16:11:53 +0530

    Added : Check employee is present or absent

--- src/com/brigelabz/Employee.java
+++ src/com/brigelabz/Employee.java
@@ -2,6 +2,12 @@ package com.brigelabz;
 
 public class Employee {
 	public static void main(String[] args) {
-			System.out.println("WelCome to Employee wages Program!!!");
+		int IS_FULL_TIME = 1;
+		int empCheck = (int)(Math.floor(Math.random() * 10) % 2);
+		 if (empCheck == IS_FULL_TIME ) {
+			 System.out.println("Employee is present");
+		 } else {
+			 System.out.println("Employee is absent");
+		 }
 	}
 }
