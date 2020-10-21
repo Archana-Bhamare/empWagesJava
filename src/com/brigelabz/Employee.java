163a957f09231cb50ae862a54d4cbed080fab5f0
Archana-Bhamare <archusonar2@gmail.com>
2020-10-21 19:03:13 +0530

    Modify : display days and emp hrs

--- src/com/brigelabz/Employee.java
+++ src/com/brigelabz/Employee.java
@@ -1,13 +1,13 @@
 package com.brigelabz;
 
 public class Employee {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+
 	public static void main(String[] args) {
-		/*
-		 * int IS_FULL_TIME = 1; int IS_PART_TIME = 2;
-		 */
-		int EMP_RATE_PER_HOUR = 20;
-		int NUM_OF_WORKING_DAYS = 20;
-		int MAX_HRS_IN_MONTH = 100;
 		int empHrs = 0;
 		int totalSalary = 0;
 		int totalEmpHrs = 0;
@@ -16,19 +16,19 @@ public class Employee {
 			totalWorkingDays++;
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 			switch (empCheck) {
-			case 1:
+			case IS_FULL_TIME:
 				empHrs = 8;
 				break;
-			case 2:
+			case IS_PART_TIME:
 				empHrs = 4;
 				break;
 			default:
 				empHrs = 0;
 				break;
 			}
-			totalEmpHrs = totalEmpHrs + empHrs;
+			totalEmpHrs += empHrs;
+			System.out.println("Days : " + totalWorkingDays + "Emp hours : " + empHrs);
 		}
-
 		totalSalary = EMP_RATE_PER_HOUR * totalEmpHrs;
 		System.out.println("Total Salary is : " + totalSalary);
 	}
