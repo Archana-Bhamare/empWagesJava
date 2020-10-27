693fab42bee74907736be24578a4374d23f3ee9d
Archana-Bhamare <archusonar2@gmail.com>
2020-10-27 16:05:39 +0530

    Added : Store Daily Wage

--- src/com/brigelabz/Employee.java
+++ src/com/brigelabz/Employee.java
@@ -1,29 +1,35 @@
 package com.brigelabz;
-import java.util.ArrayList;;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Employee implements IEmployeeWage {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
-	
 	private ArrayList<CompEmpWage> companyWageArray;
+	private HashMap<Integer,Integer> companyWageMap;
+
 	public Employee() {
 		companyWageArray = new ArrayList<>();
+		companyWageMap = new HashMap<>();
 	}
-	public void companyEmpWage(String comname,int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth){
-		CompEmpWage comWage=new CompEmpWage(comname,empRatePerHour, numOfWorkingDays, maxHoursInMonth);
+
+	public void companyEmpWage(String comname, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
+		CompEmpWage comWage = new CompEmpWage(comname, empRatePerHour, numOfWorkingDays, maxHoursInMonth);
 		companyWageArray.add(comWage);
-	
 	}
+
 	public void calculateWage() {
-		for (int i=0;i< companyWageArray.size();i++){
-			CompEmpWage companyEmpWage=companyWageArray.get(i);
+		for (int i = 0; i < companyWageArray.size(); i++) {
+			CompEmpWage companyEmpWage = companyWageArray.get(i);
 			companyEmpWage.setTotalEmpWage(this.calculateWage(companyEmpWage));
 			System.out.println(companyEmpWage);
 		}
 	}
 
 	public int calculateWage(CompEmpWage compEmpWage) {
-		 int empHrs = 0, totalEmpHrs = 0,totalWorkingDays = 0;
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0,dailyWage=0;
 		while (totalEmpHrs < compEmpWage.maxHoursInMonth && totalWorkingDays < compEmpWage.numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
@@ -38,14 +44,19 @@ public class Employee implements IEmployeeWage {
 				empHrs = 0;
 				break;
 			}
+			dailyWage = empHrs*compEmpWage.empRatePerHour;
 			totalEmpHrs += empHrs;
 			System.out.println("Days : " + totalWorkingDays + "\tEmp hours : " + empHrs);
+			companyWageMap.put(totalWorkingDays,dailyWage);
 		}
-		return totalEmpHrs*compEmpWage.empRatePerHour;
+		for (int i=0;i<companyWageMap.size();i++) {
+			System.out.println("DailyWage : " +companyWageMap.get(i));
+		}
+		return totalEmpHrs * compEmpWage.empRatePerHour;
 	}
 
 	public static void main(String[] args) {
-		Employee empArray=new Employee();
+		Employee empArray = new Employee();
 		empArray.companyEmpWage("Tech Mahindra", 15, 20, 100);
 		empArray.companyEmpWage("Infosys", 15, 20, 100);
 		empArray.calculateWage();
